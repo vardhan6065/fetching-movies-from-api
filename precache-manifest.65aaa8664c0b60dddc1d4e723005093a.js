112153acaac1d2b95318307f59e1f1f4b89dcee3
vardhan6065 <vardhandaphalea@gmail.com>
2022-05-17 14:53:18 +0530

    Updates

---
+++ precache-manifest.65aaa8664c0b60dddc1d4e723005093a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935c11084b95437255b7abdba7f70d48",
+    "url": "/fetching-movies-from-api/index.html"
+  },
+  {
+    "revision": "c695f9cb16398d1ec528",
+    "url": "/fetching-movies-from-api/static/css/main.385eb378.chunk.css"
+  },
+  {
+    "revision": "073b321df77737db16f0",
+    "url": "/fetching-movies-from-api/static/js/2.ad3588c6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/fetching-movies-from-api/static/js/2.ad3588c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c695f9cb16398d1ec528",
+    "url": "/fetching-movies-from-api/static/js/main.b27a7cc7.chunk.js"
+  },
+  {
+    "revision": "f81e94485d83975f18d9",
+    "url": "/fetching-movies-from-api/static/js/runtime-main.9235c91f.js"
+  }
+]);
